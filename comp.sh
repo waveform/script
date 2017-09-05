77e87797071fb2c930f1a3c0c6997bfeb159b6ff
Stanley Wang <Stanley.Wang@arm.com>
2017-09-05 10:48:27 -0700

    add two scripts: 1. merge multi pdf files; 2. compare bmp images

---
+++ comp.sh
@@ -0,0 +1,1 @@
+compare -metric AE -fuzz 1% rt0.out.3f437aefe15f4d83735827417257f360.bmp rt0.out.6b963b523f5a001233f72eb0694c1eb0.bmp -compose src diff1.png
---
+++ pdf_merge.sh
@@ -0,0 +1,1 @@
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=merged.pdf 4496_001.pdf 4497_001.pdf 4498_001.pdf 4499_001.pdf
